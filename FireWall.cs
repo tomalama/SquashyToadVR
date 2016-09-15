50b4705142ff2a22fc7a4560c8462081d67a88e3
Tommy Nguyen <tommy.nguyen7@hotmail.com>
2016-09-15 16:46:39 -0400

    Add CheckPlayerBurnt

--- FireWall.cs
+++ FireWall.cs
@@ -1,25 +1,31 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class FireWall : MonoBehaviour {
 
     public float creepSpeed = 0.01f;
+    private GameObject player;
+    private GameState state;
 
     // Use this for initialization
-    void Start () {
-	
-	}
+    void Start ()
+    {
+        player = GameObject.Find("Player");
+        state = GameObject.FindObjectOfType<GameState>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        FollowPlayer();
-        CreepForward();
-
+        if (!state.IsGameOver)
+        {
+            FollowPlayer();
+            CreepForward();
+            CheckPlayerBurnt();
+        }
 	}
 
     void FollowPlayer()
     {
-        GameObject player = GameObject.Find("Player");
         Vector3 delta = player.transform.position - transform.position;
         Vector3 projectedDelta = Vector3.Project(delta, Vector3.left);
         transform.position += projectedDelta;
@@ -28,5 +34,13 @@ public class FireWall : MonoBehaviour {
     void CreepForward()
     {
         transform.position += Vector3.forward * creepSpeed;
+    }
+
+    void CheckPlayerBurnt()
+    {
+        if(player.transform.position.z < transform.position.z)
+        {
+            state.IsGameOver = true;
+        }
     }
 }
