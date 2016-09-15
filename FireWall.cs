6d8fbb4c21304b2dbee08671d6ed53fb1acd675d
Tommy Nguyen <tommy.nguyen7@hotmail.com>
2016-09-15 16:52:35 -0400

    Update FireWall.cs

--- FireWall.cs
+++ FireWall.cs
@@ -1,46 +1,48 @@
 using UnityEngine;
 using System.Collections;
 
-public class FireWall : MonoBehaviour {
-
-    public float creepSpeed = 0.01f;
-    private GameObject player;
-    private GameState state;
+public class FireWall : MonoBehaviour 
+{
+	
+	public float creepSpeed = 0.01f;
+    	private GameObject player;
+    	private GameState state;
 
     // Use this for initialization
-    void Start ()
-    {
-        player = GameObject.Find("Player");
-        state = GameObject.FindObjectOfType<GameState>();
-    }
+    	void Start ()
+    	{
+        	player = GameObject.Find("Player");
+        	state = GameObject.FindObjectOfType<GameState>();
+    	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (!state.IsGameOver)
-        {
-            FollowPlayer();
-            CreepForward();
-            CheckPlayerBurnt();
-        }
+	void Update () 
+	{
+        	if (!state.IsGameOver)
+        	{
+            		FollowPlayer();
+            		CreepForward();
+            		CheckPlayerBurnt();
+        	}
 	}
 
-    void FollowPlayer()
-    {
-        Vector3 delta = player.transform.position - transform.position;
-        Vector3 projectedDelta = Vector3.Project(delta, Vector3.left);
-        transform.position += projectedDelta;
-    }
+    	void FollowPlayer()
+    	{
+        	Vector3 delta = player.transform.position - transform.position;
+        	Vector3 projectedDelta = Vector3.Project(delta, Vector3.left);
+        	transform.position += projectedDelta;
+    	}
 
-    void CreepForward()
-    {
-        transform.position += Vector3.forward * creepSpeed;
-    }
+    	void CreepForward()
+    	{
+        	transform.position += Vector3.forward * creepSpeed;
+    	}
 
-    void CheckPlayerBurnt()
-    {
-        if(player.transform.position.z < transform.position.z)
-        {
-            state.IsGameOver = true;
-        }
-    }
+    	void CheckPlayerBurnt()
+    	{
+        	if(player.transform.position.z < transform.position.z)
+        	{
+            		state.IsGameOver = true;
+        	}
+    	}
 }
