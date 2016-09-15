2ec1aa00fdf9bdaa8c310abe240894c8cea62e87
Tommy Nguyen <tommy.nguyen7@hotmail.com>
2016-09-15 17:02:13 -0400

    Update VehicleSpawner.cs

--- VehicleSpawner.cs
+++ VehicleSpawner.cs
@@ -2,48 +2,47 @@
 using System.Collections;
 
 public class VehicleSpawner : MonoBehaviour {
+	
+	public GameObject[] vehiclePrefabs;
+    	public float heightOffset = 1f;
+    	public float startOffset = -10f;
+	public float speed = 5.0f;
+	public float length = 10.0f;
+	public float maxSpawnTime = 10f;
 
-    public GameObject[] vehiclePrefabs;
-    public float heightOffset = 1f;
-    public float startOffset = -10f;
-    public float speed = 5.0f;
-    public float length = 10.0f;
-    public float maxSpawnTime = 10f;
+    	private Vector3 positionOffset;
+    	private int randomLaneIndex;
 
-    private Vector3 positionOffset;
-    private int randomLaneIndex;
-
-    void Start ()
-    {
-        StartCoroutine("Spawn");
+    	void Start ()
+    	{
+        	StartCoroutine("Spawn");
 	}
 
-    IEnumerator Spawn()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(Random.Range(1f, maxSpawnTime));
-            InstantiateVehicle();
-        }
-    }
+    	IEnumerator Spawn()
+    	{
+        	while (true)
+        	{
+            		yield return new WaitForSeconds(Random.Range(1f, maxSpawnTime));
+            		InstantiateVehicle();
+        	}
+    	}
 
-    Vector3 GetPositionOffset()
-    {
-        positionOffset = transform.position;
-        positionOffset += heightOffset * Vector3.up;
-        positionOffset += startOffset * Vector3.right;
-        return positionOffset;
-    }
+    	Vector3 GetPositionOffset()
+    	{
+        	positionOffset = transform.position;
+        	positionOffset += heightOffset * Vector3.up;
+        	positionOffset += startOffset * Vector3.right;
+        	return positionOffset;
+    	}
+    	
+    	void InstantiateVehicle()
+    	{
+        	randomLaneIndex = Random.Range(0, vehiclePrefabs.Length);
+        	GameObject vehicleObject = Instantiate(vehiclePrefabs[randomLaneIndex]);
+        	vehicleObject.transform.position = GetPositionOffset();
+        	vehicleObject.transform.parent = transform;
 
-    void InstantiateVehicle()
-    {
-        randomLaneIndex = Random.Range(0, vehiclePrefabs.Length);
-        GameObject vehicleObject = Instantiate(vehiclePrefabs[randomLaneIndex]);
-        vehicleObject.transform.position = GetPositionOffset();
-        vehicleObject.transform.parent = transform;
-
-        Vehicle vehicleComponent = vehicleObject.GetComponent<Vehicle>();
-        vehicleComponent.SetPath(speed, length);
-
-    }
+        	Vehicle vehicleComponent = vehicleObject.GetComponent<Vehicle>();
+        	vehicleComponent.SetPath(speed, length);
+    	}
 }
