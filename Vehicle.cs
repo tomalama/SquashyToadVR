8ee08a8891dc16e3b09f5741edc6efa36d940147
Tommy Nguyen <tommy.nguyen7@hotmail.com>
2016-09-15 16:58:53 -0400

    Add files via upload

---
+++ Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vehicle : MonoBehaviour {
+
+    private float speed;
+    private float length;
+
+	void Start ()
+    {
+        float lifeTime = length / speed;
+        Invoke("Remove", lifeTime);
+	}
+	
+    void Remove()
+    {
+        Destroy(gameObject);
+    }
+
+	void Update ()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+
+    public void SetPath (float someSpeed, float someLength)
+    {
+        speed = someSpeed;
+        length = someLength;
+    }
+}
---
+++ VehicleSpawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleSpawner : MonoBehaviour {
+
+    public GameObject[] vehiclePrefabs;
+    public float heightOffset = 1f;
+    public float startOffset = -10f;
+    public float speed = 5.0f;
+    public float length = 10.0f;
+    public float maxSpawnTime = 10f;
+
+    private Vector3 positionOffset;
+    private int randomLaneIndex;
+
+    void Start ()
+    {
+        StartCoroutine("Spawn");
+	}
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1f, maxSpawnTime));
+            InstantiateVehicle();
+        }
+    }
+
+    Vector3 GetPositionOffset()
+    {
+        positionOffset = transform.position;
+        positionOffset += heightOffset * Vector3.up;
+        positionOffset += startOffset * Vector3.right;
+        return positionOffset;
+    }
+
+    void InstantiateVehicle()
+    {
+        randomLaneIndex = Random.Range(0, vehiclePrefabs.Length);
+        GameObject vehicleObject = Instantiate(vehiclePrefabs[randomLaneIndex]);
+        vehicleObject.transform.position = GetPositionOffset();
+        vehicleObject.transform.parent = transform;
+
+        Vehicle vehicleComponent = vehicleObject.GetComponent<Vehicle>();
+        vehicleComponent.SetPath(speed, length);
+
+    }
+}
