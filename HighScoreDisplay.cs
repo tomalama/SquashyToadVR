f580df24f4270723242b8499059e8797517cc429
Tommy Nguyen <tommy.nguyen7@hotmail.com>
2016-09-16 12:14:37 -0400

    Add files via upload

---
+++ HighScoreDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HighScoreDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameState state = GameObject.FindObjectOfType<GameState>();
+        Text text = GetComponent<Text>();
+        text.text = "High Score: " + state.highScore + " meters";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
